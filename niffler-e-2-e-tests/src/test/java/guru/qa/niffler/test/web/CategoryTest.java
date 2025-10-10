de4334b2d2ba77ab0e7b7be53663d139127f9e9f
IlyaLesnikov <lialesnikovqa1999@gmail.com>
2025-10-11 00:11:44 +0300

    lesson_3_3 пофиксил конфликтя после ребейза

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/CategoryTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/CategoryTest.java
@@ -5,7 +5,6 @@ import guru.qa.niffler.config.Config;
 import guru.qa.niffler.jupiter.annotation.Category;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.WebTest;
-import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.page.LoginPage;
 import org.junit.jupiter.api.DisplayName;
