8571e523196a43d7912218e739c83b854d0caf71
IlyaLesnikov <lialesnikovqa1999@gmail.com>
2025-10-11 00:02:49 +0300

    lesson_3_2 поправил автотесты после ревью

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/CategoryTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/CategoryTest.java
@@ -3,6 +3,7 @@ package guru.qa.niffler.test.web;
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.jupiter.annotation.Category;
+import guru.qa.niffler.jupiter.annotation.WebTest;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.WebTest;
 import guru.qa.niffler.model.CategoryJson;
