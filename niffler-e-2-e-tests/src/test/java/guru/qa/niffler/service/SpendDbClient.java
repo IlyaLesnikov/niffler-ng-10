f9bc4b97da17bbd2fdb2b1cadef2498eac15dcff
IlyaLesnikov <lialesnikovqa1999@gmail.com>
2025-10-19 14:37:56 +0300

    lesson_5_1 написал SpendDao и CategoryDao на Spring JDBC

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
@@ -26,7 +26,6 @@ public class SpendDbClient implements SpendClient {
       CategoryEntity categoryEntity = categoryDao.create(spendEntity.getCategory());
       spendEntity.setCategory(categoryEntity);
     }
-
     return SpendJson.fromEntity(spendDao.create(spendEntity));
   }
 
